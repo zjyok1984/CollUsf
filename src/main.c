34f10ed4d495b5d523c8789124937ca4b0172354
建雨 张 <zjyok1984@163.com>
2019-05-30 10:07:26 +0800

    Common change.

--- src/main.c
+++ src/main.c
@@ -2,11 +2,19 @@
 #include	<string.h>
 #include	"collusf.h"
 
+#define	_CRT_SECURE_NO_WARNINGS
+
+
 int main(int argc, char* argv[])
 {
+	char Temp[256];
 	if (argc > 1)
-		printf("%s\t%s", argv[0], argv[1]);
+	{
+		printf("%s\t%s\n", argv[0], argv[1]);
+		strcpy(Temp, argv[0]);
+		strcat(Temp, argv[1]);
+		printf("%s\n", Temp);
+	}
 	else
-		printf("%s", argv[0]);
-
+		printf("%s\n", argv[0]);
 }
