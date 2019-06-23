161e07a68ba050a600ae5ada087494b594038e6d
Jianyu-PC <zjyok1984@163.com>
2019-06-23 22:55:53 +0800

    增加初始化和循环调用

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 ﻿#include	<stdio.h>
 #include	<string.h>
+#include	"cJSON.h"
 #include	"collusf.h"
+
+/*初始化汇总调用*/
+static void ProcInit()
+{
+	return;
+}
+
+/*循环汇总调用*/
+static void ProcLoop()
+{
+	return;
+}
 
 int main(int argc, char* argv[])
 {
 	CommonRun(argc, argv);
+	ProcInit();
+	ProcLoop();
 }
